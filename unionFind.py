dfb6186fb8e35b3a1ab65f5bcf860ffee2f4dd1a
Aadish Goel <aadishgoel2013@users.noreply.github.com>
2023-04-29 04:50:37 +0530

    Add files via upload

---
+++ unionFind.py
@@ -0,0 +1,63 @@
+############  O(1)-O(n) Map-List ##################
+class UnionFind1:
+    def __init__(self, n):
+        self.gp = { i:i for i in range(n)}
+        self.sets = { i:[i] for i in range(n) }
+    
+    def union(self,i,j):
+        if self.gp[i] != self.gp[j]:
+            temp = self.gp[j]
+            for node in self.sets[ self.gp[j] ]:
+                self.gp[node] = self.gp[i]
+                self.sets[self.gp[i]] += [node]
+            del self.sets[temp]
+
+    def find(self,i):
+        return self.gp[i]
+    
+
+############  O(logn)-O(logn) Tree ##################
+
+class UnionFind2:
+    def __init__(self,n) -> None:
+        self.parent = [i for i in range(n)]
+    
+    def find(self, i):
+        if self.parent[i] == i:
+            return i
+        return find(self.parent[i])
+     
+    def union(self, i,j): 
+        self.parent[ self.find(j) ] =  self.find(i)
+    
+        
+############  O(1)-O(1) Tree-Rank-PathCompression ##################
+
+class UnionFind3:
+    def __init__(self, n):
+        self.rank = [0 for i in range(n)]
+        self.parent = [i for i in range(n)]
+    
+    def find(self, i):
+        if self.parent[i]==i:
+            return i
+        self.parent[i] = find(self.parent[i])
+        return self.parent[i]
+    
+    def union(self, i,j):
+        u = self.find(i)
+        v = self.find(j)
+        if self.rank[u]<self.rank[v]:
+            self.parent[v]=u
+        elif self.rank[v]<self.rank[u]:
+            self.parent[u]=v
+        else:
+            self.parent[v]=u
+            self.rank[u]+=1
+    
+
+
+uf = UnionFind3(5)
+uf.union(1,2)
+uf.union(3,4)
+print(uf.parent)
